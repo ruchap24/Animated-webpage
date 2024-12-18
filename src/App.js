b341e57c3092b045bcac43ea4f1aedea25a285a0
ruchap24 <patilrucha991@gmail.com>
2024-12-18 15:45:12 +0530

    animation added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,67 @@
-
+import React, { useEffect,useRef } from "react";
 import './App.css';
+import anime from 'animejs'
 
 function App() {
+  const containerRef= useRef(null);
+  useEffect(()=>{
+    const container=containerRef.current;
+    container.innerHTML="";
+
+    for(let i=1;i<=100;i++){
+      let dot=document.createElement("div");
+      dot.classList.add("element")
+      container.appendChild(dot);
+    }
+    let dotAll = container.querySelectorAll(".element");
+    let animation=anime.timeline({
+      targets : dotAll,
+      easing: "easeInOutExpo",
+      loop: true,
+      delay: anime.stagger(100, { grid:[10,10], from: "center" }),
+    });
+
+    //first animation
+
+    animation
+    .add({
+      rotateZ:180,
+      translateY: anime.stagger(0, { grid:[10,10],from:"center", axis:"y"}),
+      translateX: anime.stagger(0, { grid:[10,10],from:"center", axis:"x"}),
+      opacity:1,
+    })
+
+    .add({
+      borderRadius: 50,
+    })
+
+    .add({
+      scale: 0.2,
+      opacity: 0.2,
+    })
+
+    //second animation
+
+    .add({
+      rotateZ:180,
+      translateY: anime.stagger(0, { grid:[10,10],from:"center", axis:"y"}),
+      translateX: anime.stagger(0, { grid:[10,10],from:"center", axis:"x"}),
+      opacity:1,
+    })
+
+    .add({
+      scale:1,
+      borderRadius: 0,
+    })
+
+    .add({
+      rotateZ: -90,
+    });
+
+
+  },[])
+  
+
   return (
     <div>
       {/* header section start */}
@@ -38,6 +98,7 @@ function App() {
 
       {/* section hero */}
       <section>
+        {/* content area start */}
         <div className='content'>
           <h2>
             Web Page Using <b>Anime.js</b>
@@ -50,6 +111,10 @@ function App() {
             Learn More
             </a>
         </div>
+        {/* content area end */}
+
+        {/* animated area */}
+        <div className='container' ref={containerRef}></div>
       </section>
 
     </div>
